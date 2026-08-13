7ff8a362715449dd3e169e70df4984ffbe866aaa
charity kalu <charitykalu825@gmail.com>
2026-08-13 21:00:47 +0100

    Test: verify CI plan workflow

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -449,3 +449,33 @@ resource "aws_iam_user_policy" "dev_view_s3_put" {
     }]
   })
 }
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "bedrock-github-actions"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Federated = aws_iam_openid_connect_provider.github.arn }
+      Action    = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:CHARITY133/project-bedrock:*"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}# CI/CD pipeline test - verifying plan-on-PR workflow
